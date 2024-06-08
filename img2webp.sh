5f181bd49fce16101999c0af4e3d09ca2fd8bdb2
春木Haruki <harukiinharu@gmail.com>
2024-06-09 00:40:21 +0800

    use cwebp command

---
+++ img2webp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+folder_list=("public/images" "lyric")
+
+for folder in "${folder_list[@]}"; do
+    for file in $folder/*.png; do
+        cwebp $file -q 50 -o ${file%.png}.webp -quiet
+        rm $file
+    done
+done
